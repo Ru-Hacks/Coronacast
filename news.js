8c9457037e8181fe515f030e6f2af82edddd51f4
Krishaanth <krish100@my.yorku.ca>
2020-05-16 19:10:08 -0400

    Trying Newsapi

--- news.js
+++ news.js
@@ -8,12 +8,19 @@ fetch(req)
         console.log(response.json());
     })
 */
-document.getElementById("news1").addEventListener("click", getNews);
+
+// var el = document.getElementById('news1');
+// if(el){
+//   el.addEventListener('click', getNews);
+// }
+
+//document.getElementById("news1").addEventListener("click", getNews);
 function getNews(){
 
     var url ='http://newsapi.org/v2/everything?' +
-    'q=Apple&' +
-    'from=2020-05-16&' +
+    'q=Coronavirus Cases&' +
+    'domains=nytimes.com&' +
+    'from=2020-05-00&' +
     'sortBy=popularity&' +
     'apiKey=f53bb7e9ed3545deb75d7215f07dca25';
 
@@ -21,7 +28,8 @@ function getNews(){
 
     fetch(req)
         .then(function(response) {
-            let data = response.json;
+            let data = response.json();
+            console.log("poopy");
             console.log(data);
 
         });
