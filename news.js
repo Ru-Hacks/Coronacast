d207cc2f0a91f3ea7f21cbfb2a4effa8f6b04658
Krishaanth <krish100@my.yorku.ca>
2020-05-16 18:10:23 -0400

    Create news.js

---
+++ news.js
@@ -0,0 +1,10 @@
+/*
+var url = 'http://newsapi.org/v2/top-headlines?' +
+          'country=us&' +
+          'apiKey=f53bb7e9ed3545deb75d7215f07dca25';
+var req = new Request(url);
+fetch(req)
+    .then(function(response) {
+        console.log(response.json());
+    })
+*/
