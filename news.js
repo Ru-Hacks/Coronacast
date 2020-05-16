fbacb483bbb29b1822f0821b43d6ec634794bd2a
Krishaanth <krish100@my.yorku.ca>
2020-05-16 18:28:59 -0400

    Trying News

--- news.js
+++ news.js
@@ -8,3 +8,23 @@ fetch(req)
         console.log(response.json());
     })
 */
+document.getElementById("news1").addEventListener("click", getNews);
+function getNews(){
+
+    var url ='http://newsapi.org/v2/everything?' +
+    'q=Apple&' +
+    'from=2020-05-16&' +
+    'sortBy=popularity&' +
+    'apiKey=f53bb7e9ed3545deb75d7215f07dca25';
+
+    var req = new Request(url);
+
+    fetch(req)
+        .then(function(response) {
+            let data = response.json;
+            console.log(data);
+
+        });
+
+
+}
