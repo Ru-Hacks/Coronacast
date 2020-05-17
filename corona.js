5f00dc58b535a7ba64b98f120dce999996f5d9c5
David Chung <david23@my.yorku.ca>
2020-05-16 20:42:06 -0400

    Delete corona.js

--- corona.js
+++
@@ -1,9 +0,0 @@
-var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-};
-
-fetch("https://thevirustracker.com/free-api?global=stats", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
