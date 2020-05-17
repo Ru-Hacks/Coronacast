96605bd5f030d39a1175802e4bbcb939ce4431da
Krishaanth <krish100@my.yorku.ca>
2020-05-16 20:38:59 -0400

    Fixed Transit

--- news.js
+++ news.js
@@ -17,12 +17,13 @@ fetch(req)
 //document.getElementById("news1").addEventListener("click", getNews);
 function getNews(){
 
-    var url ='http://newsapi.org/v2/everything?' +
-    'q=Coronavirus Cases&' +
-    'domains=nytimes.com&' +
-    'from=2020-05-00&' +
-    'sortBy=popularity&' +
-    'apiKey=f53bb7e9ed3545deb75d7215f07dca25';
+    // var url ='http://newsapi.org/v2/everything?' +
+    // 'q=Corona&' +
+    // 'from=2020-05-00&' +
+    // 'sortBy=popularity&' +
+    // 'apiKey=f53bb7e9ed3545deb75d7215f07dca25';
+
+    var url = 'https://newsapi.org/v2/top-headlines?q=Corona&country=ca&apiKey=f53bb7e9ed3545deb75d7215f07dca25';
 
     var req = new Request(url);
 
