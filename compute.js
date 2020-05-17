6a4de553fc6141d83ffc42c9485db365408bcff6
Arjun Kaura <32141878+arjunKa@users.noreply.github.com>
2020-05-16 20:22:22 -0400

    style and index and compute

--- compute.js
+++ compute.js
@@ -9,6 +9,12 @@ var temp = {
 
 
 };
+var age = 60;
+//---------------
+
+
+
+//---------------
 
 var humidityLocal = localStorage.getItem("vOneLocalStorage");  
 var description;
@@ -25,11 +31,13 @@ else
     humidity.prop = "high";
 
 
-humidity.result= "The humidity is " + humidity.prop + ". It is " + humidity.value;
+humidity.result= "The weather is " +  weather.description + ". The humidity is " +
+ humidity.prop + ". It is " + humidity.value;
 
 
 description = humidity.result;
     
-
+document.getElementById("percent").innerHTML = "23%";
 document.getElementById("description").innerHTML = "Desc: " + description;
+
 }
